1f77c57004dd1ff00a43c932f0c28cbf5a69a50a
Taranveer <taranveersingh12@gmail.com>
2016-02-17 19:11:59 +0530

    clean sent_rule file of opinion miner

--- Subjective_Clues/clean_data.py
+++ Subjective_Clues/clean_data.py
@@ -4,12 +4,17 @@ import pandas as pd
 import re
 import csv
 from nltk.corpus import stopwords
-with open('ObjectiveNews1.json') as data_file:    
+
+#Read input json file for cleaning
+input_file=raw_input("input file--->")
+#Setting path for input file
+input_file="Data/json/" + input_file
+with open(input_file) as data_file:    
      data = json.load(data_file)
 
-# sentences = pd.read_csv("SubjectiveNews1.json", sep="\t")
-# print sentences
 
+
+#-----------Resolve encoding problem------------------------------------------#
 unicodeToAsciiMap = {u'\u2019':"'", u'\u2018':"`", }
 
 def unicodeToAscii(inStr):
@@ -29,21 +34,27 @@ def unicodeToAscii(inStr):
     return outStr
 
 
-
+#-------------Clean data-tokenize-filter--------------------------------------#
 def clean_data():
 	content_all=""
+	#---------extract text of news articles from given JSON and merge all-----#
 	for i in range(1,len(data)):
 		content_all=content_all + " " + data[i]['value']
 	content_all=unicodeToAscii(content_all)
 	tokens=nltk.word_tokenize(content_all)
-	#--------- remove stopwords----------#
+	#---------remove stopwords-----------------------------------------------#
 	letters_only = re.sub("[^a-zA-Z]", " ", content_all) 
 	words = letters_only.lower().split() 
 	stops = set(stopwords.words("english"))
 	meaningful_words = [w for w in words if not w in stops]
+	#exclude one letter or two letter words that occur due to parsing issues
 	meaningful_words = [w for w in meaningful_words if len(w)>2]
 	print meaningful_words
-	with open('ObjectiveNews.csv', 'wb') as f:
+	#Name of csv file for output
+	output_file=raw_input("output_file--->")
+	#Setting path for output file
+	output_file="Data/csv/" + output_file + ".csv"
+	with open(output_file, 'wb') as f:
 		writer = csv.writer(f)
 		for val in meaningful_words:
 			writer.writerow([val])
