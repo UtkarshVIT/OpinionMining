14fe06a233ae6be37dbbbd239d2211325a7874b6
Taranveer <taranveersingh12@gmail.com>
2016-03-03 18:17:02 +0530

    Changes made to extract_rule

--- Subjective_Clues/extract_rules.R
+++ Subjective_Clues/extract_rules.R
@@ -13,13 +13,17 @@
   word_database$tags=0
   #Don't forget to exclude # words
   #Read sentences file from news articles
-  sentences=read.csv("SubjectiveNews.csv",header= F)
+  sentences_subj=read.csv("SubjectiveNews.csv",header= F)
+  sentences_obj=read.csv("ObjectiveNews.csv",header= F)
   #Give name to col containing words
-  colnames(sentences)="words"
+  colnames(sentences_subj)="words"
+  colnames(sentences_obj)="words"
   #Convert toupper to match inquirer dataset
-  sentences$words=toupper(sentences$words)
+  sentences_subj$words=toupper(sentences_subj$words)
+  sentences_obj$words=toupper(sentences_obj$words)
   #Find common words
-  common_words=intersect(sentences$words,word_database$Entry)
+  common_words_subj=intersect(sentences_subj$words,word_database$Entry)
+  common_words_obj=intersect(sentences_obj$words,word_database$Entry)
   #del words with tags in exclude
   del_words=c()
   for (i in 1:length(exclude))
@@ -28,16 +32,38 @@
   }
   del_words=del_words[complete.cases(del_words)]
   print (del_words)
-  common_words=setdiff(common_words,del_words)
-  print("common words")
-  print (common_words)
+  common_words_subj=setdiff(common_words_subj,del_words)
+  common_words_obj=setdiff(common_words_obj,del_words)
+  print("common words_subj")
+  print (common_words_subj)
+  print("common words_obj")
+  print (common_words_subj)
+  
   #Create a temp data frame to store tags related to each word
   temp_subj=word_database[1,]
   #loop to get tags of words present in news articles(sentences)
-  for (i in 2:length(common_words))
+  for (i in 2:length(common_words_subj))
   {
     tags=c()
     temp_subj[i,]=word_database[word_database$Entry==common_words[i],]
+    for(j in 2:(ncol(temp_subj)-1) )
+    { 
+      if(temp_subj[i,j]!="")
+      {
+        tags=append(tags,colnames(temp_subj)[j])
+      }
+      
+    }
+    tags=append(tags,"Subjective")
+    temp_subj[i,]$tags=list(tags)
+    #print (temp[i,]$tags)
+  }
+  
+  View(temp_subj)
+  for (i in 2:length(common_words_obj))
+  {
+    tags=c()
+    temp_obj[i,]=word_database[word_database$Entry==common_words[i],]
     for(j in 2:(ncol(temp_subj)-1) )
     { 
       if(temp_subj[i,j]!="")
